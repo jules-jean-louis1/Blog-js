26d66217f5fb158df14495dd8c03117bba269e49
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-03-06 11:05:32 +0100

    1er push

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Blog</title>
+</head>
+<body>
+<header>
+    <?php include_once 'resources/assests/import/header.php' ?>
+</header>
+    <main>
+
+    </main>
+</body>
+</html>
---
+++ resources/assests/import/header.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<div id="containerHeader">
+    <div id="wapperHeader">
+        <div id="logo"></div>
+        <div id="linksHeader">
+            <div id="navbar">
+                <nav>
+                    <li>
+                        <a href="">Inscription</a>
+                    </li>
+                    <li>
+                        <a href="">Connexion</a>
+                    </li>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
