fa1399c1e749da9879a43843ff4fa20cd36360fc
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-03-06 11:17:56 +0100

    add form

---
+++ resources/assests/fetch/login.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<form action="" method="post" id="login-form">
+    <div id="containerLogin">
+        <label for="login">Email</label>
+        <input type="text" name="login" id="login" placeholder="login">
+    </div>
+    <div id="containerPassword">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="password">
+    </div>
+    <div id="displayMessage"></div>
+    <div id="containerSubmit">
+        <button type="submit" name="submit" id="submit">Connexion</button>
+    </div>
+</form>
---
+++ resources/assests/fetch/register.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<form action="" method="post" id="resgister-form">
+    <div id="containerlogin">
+        <label for="login">Login</label>
+        <input type="text" name="login" id="login" placeholder="login">
+    </div>
+    <div id="containerPassword">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="password">
+    </div>
+    <div id="containerPasswordConfirm">
+        <label for="passwordConfirm">Confirmer le mot de passe</label>
+        <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="passwordConfirm">
+    </div>
+    <div id="displayMessage"></div>
+    <div id="containerSubmit">
+        <button type="submit" name="submit" id="submit">Inscription</button>
+    </div>
+</form>
