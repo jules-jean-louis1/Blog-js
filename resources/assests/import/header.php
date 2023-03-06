06b753af90a980bd55fa2aeea9d9c5d073f6643d
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-03-06 23:47:24 +0100

    add modif co

--- resources/assests/import/header.php
+++ resources/assests/import/header.php
@@ -7,16 +7,31 @@
         <div id="logo"></div>
         <div id="linksHeader">
             <div id="navbar">
+                <?php if (isset($_SESSION['login']) != null) : ?>
                 <nav>
                     <ul class="flex space-x-2">
                         <li>
-                            <button type="button" id="buttonRegisterHeader" class="p-2 bg-blue-400">Inscription</button>
+                            <button type="button" id="buttonRegisterHeader" class="p-2 bg-blue-400">
+                                <a href="pages/profil.php"><?= $_SESSION['login'] ?></a>
+                            </button>
                         </li>
                         <li>
                             <button type="button" id="buttonLoginHeader" class="p-2 bg-blue-200">Connexion</button>
                         </li>
                     </ul>
                 </nav>
+                <?php else : ?>
+                    <nav>
+                        <ul class="flex space-x-2">
+                            <li>
+                                <button type="button" id="buttonRegisterHeader" class="p-2 bg-blue-400">Inscription</button>
+                            </li>
+                            <li>
+                                <button type="button" id="buttonLoginHeader" class="p-2 bg-blue-200">Connexion</button>
+                            </li>
+                        </ul>
+                    </nav>
+                <?php endif; ?>
             </div>
         </div>
     </div>
