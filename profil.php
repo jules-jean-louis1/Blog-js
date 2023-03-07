143927724fc1859396ced267e2678816b83ecbd8
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-03-07 16:31:52 +0100

    add page profil

---
+++ profil.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+?>
+<?php if (isset($_SESSION['login']) != null) :?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="resources/js/profil.js"></script>
+    <link rel="stylesheet" href="resources/style/index.css">
+    <title><?= $_SESSION['login']?> - Profil</title>
+</head>
+<body>
+<header>
+    <?php include_once 'resources/assests/import/header.php' ?>
+</header>
+<main>
+
+</main>
+</body>
+</html>
+<?php else : ?>
+    <?php header('Location: index.php') ?>
+<?php endif; ?>
